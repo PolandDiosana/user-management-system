85639d29f3ea337ee7a322a5f7dcc5ac39fd495c
Harold Nabua <stratstrat120@gmail.com>
2025-05-19 20:03:59 +0800

    frontend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://dbhelper-nabua.onrender.com'
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://dbhelper-nabua.onrender.com'
+};
